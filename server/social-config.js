ca20de07b7cfa80983bf9f9694e6d38f155c7a58
Mohamed Saleem <mohsal.saleem@gmail.com>
2015-09-22 19:03:30 +0530

    initial fb auth completion

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+	service:'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+	service:'facebook',
+	appId: '492211520953440',
+	secret: '5cc84ffc8276c600d726b1726be2c22f'
+});
